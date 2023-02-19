b561fceb701080b3e08eb46ad520e2d1053f7048
IJ <jawaid_imran@hotmail.com>
2023-02-19 15:10:40 +0000

    Rough page structure

---
+++ Template/config/cleaner.php
@@ -0,0 +1,18 @@
+<div class="page-margin">
+    <div class="page-header">
+        <h2 class="">Content Cleaner</h2>
+    </div>
+    <p class="cleaner-intro">
+        
+    </p>
+    <div class="cleaner-warning">
+        <h4 class="">Warning</h4>
+        <p class="">Using this tool deletes data from the database permanently</p>
+    </div>
+    <fieldset>
+        <legend>Application Cleaning</legend>
+    </fieldset>
+    <fieldset>
+        <legend>Plugin Cleaner</legend>
+    </fieldset>
+</div>
