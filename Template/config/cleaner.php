1d8aa42bb89bb36bcdd1b11ef3824bd2f3a8a9d0
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-03-11 02:03:50 +0000

    Add Unit `MB`

--- Template/config/cleaner.php
+++ Template/config/cleaner.php
@@ -36,7 +36,7 @@
                     <?php if (DB_DRIVER === 'sqlite'): ?>
                         <td class=""><?= $this->text->bytes($db_size) ?></td>
                     <?php else: ?>
-                        <td class=""><?= $this->helper->cleaningHelper->dbSize() ?></td>
+                        <td class=""><?= $this->helper->cleaningHelper->dbSize() ?> MB</td>
                     <?php endif ?>
                 </tr>
             </table>
