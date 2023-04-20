4eeb8e826b0472d850ab5dcda064f3b4a0ab877e
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-20 20:06:37 +0100

    Fix Schema Filenames

--- Helper/DefaultTableHelper.php
+++ Helper/DefaultTableHelper.php
@@ -246,17 +246,17 @@ class DefaultTableHelper extends Base
 
     private function getSchema()
     {
-        switch (DB_DRIVER) { // For now, I am switching everything to reading the Mysql.php file.
+        switch (DB_DRIVER) {
             case 'sqlite':
-                return $_SERVER['DOCUMENT_ROOT'].'/app/Schema/Mysql.php';
+                return $_SERVER['DOCUMENT_ROOT'].'/app/Schema/Sqlite.php';
             case 'postgres':
-                return $_SERVER['DOCUMENT_ROOT'].'/app/Schema/Mysql.php';
+                return $_SERVER['DOCUMENT_ROOT'].'/app/Schema/Postgres.php';
             case 'mssql':
-                return $_SERVER['DOCUMENT_ROOT'].'/app/Schema/Mysql.php';
+                return $_SERVER['DOCUMENT_ROOT'].'/app/Schema/Mssql.php';
             case 'mysql':
                 return $_SERVER['DOCUMENT_ROOT'].'/app/Schema/Mysql.php';
             default:
-                return $_SERVER['DOCUMENT_ROOT'].'/app/Schema/Mysql.php';
+                return $_SERVER['DOCUMENT_ROOT'].'/app/Schema/Sqlite.php';
         }
     }
 }
