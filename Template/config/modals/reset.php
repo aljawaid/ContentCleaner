da3f40883566b6d08116e0b39b2ba941e107111c
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-21 19:01:34 +0100

    Update reset.php

--- Template/config/modals/reset.php
+++ Template/config/modals/reset.php
@@ -7,19 +7,25 @@
     <h2 class="relative">
         <span class="content-cleaner-icon"></span> <?= t('Confirm Cleaning Job N° ') . $job ?>
         <span class="modal-title">
-            <?php if (($incomingController == 'CleaningController') && ($outgoingAction =='confirmReset')): ?>
-                <span class="db-settings-icon"></span> <?= t('Reset Settings') ?> <span class="modal-template-id"></span>
+            <?php if (($incomingController == 'CleaningController') && ($outgoingAction =='confirmResetCalendar')): ?>
+                <span class="db-settings-icon"></span> <?= t('Reset Settings') ?> <span class="modal-reset-settings"></span>
             <?php endif ?>
         </span>
     </h2>
 </div>
-<div id="DeleteModal" class="modal-contents">
+<div id="ResetModal" class="modal-contents">
     <div class="confirm">
-        <?php if (($incomingController == 'CleaningController') && ($outgoingAction =='confirmReset')): ?>
+        <?php if (($incomingController == 'CleaningController') && ($outgoingAction =='confirmResetCalendar')): ?>
             <p class="confirm-notice">
-                <?= t('Click the button to restore the calendar settings to the default values in the \'%s\' table', $table) ?>
+                <?= t('Click the button to restore the calendar settings to the default values') ?>
+                <ul class="job-tables fa-ul">
+                    <li class="job-tables-title">
+                        <i class="fa fa-table fa-li" aria-hidden="true"></i><?= t('Table Affected') ?>
+                    </li>
+                    <li class="job-tables-table"><?= $table ?></li>
+                </ul>
             </p>
-            <?= $this->modal->confirmButtons('CleaningController', 'resetCalendarSettings', array('plugin' => 'ContentCleaner'), t('Reset')) ?>
+            <?= $this->modal->confirmButtons('CleaningController', 'resetCalendarSettings', array('plugin' => 'ContentCleaner'), t('Process Job')) ?>
         <?php endif ?>
     </div>
 </div>
