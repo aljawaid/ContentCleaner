40079a14d8b3f4418b188d8611fbd0eeedc7be08
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-22 22:40:55 +0100

    Add `Remember Me` Cleaning Job to Template

--- Template/cleaning-jobs/core-clean.php
+++ Template/cleaning-jobs/core-clean.php
@@ -65,3 +65,42 @@
         </div>
     </fieldset>
 </div>
+
+<!-- ContentCleaner: CLEANING JOB 03 -->
+<div class="job-section-wrapper">
+    <fieldset class="job-wrapper relative">
+        <legend class="job-title">
+            <span class="content-cleaner-icon"></span> <?= t('Remember Me Login Sessions') ?>
+            <!-- ContentCleaner: DO NOT CHANGE JOB NUMBERS -->
+            <span class="job-number-wrapper" title="<?= t('Cleaning Job Number') ?>"><span class="job-number">03</span></span>
+        </legend>
+        <p class="job-desc">
+            <?= t('By default, entries are automatically deleted after 60 days. Delete all entries for all users. ') ?>
+        </p>
+        <div class="job-content">
+            <ul class="job-tables fa-ul">
+                <li class="job-tables-title">
+                    <i class="fa fa-table fa-li" aria-hidden="true"></i><?= t('Table Affected') ?>
+                </li>
+                <li class="job-tables-table">remember_me</li>
+            </ul>
+            <ul class="job-table-size fa-ul">
+                <li class="job-tables-title">
+                    <i class="fa fa-table fa-li" aria-hidden="true"></i><?= t('Table Contents') ?>
+                </li>
+                <li class="job-tables-data">
+                    <span class="table-data-info">
+                        <?= $this->helper->cleaningHelper->countRememberMe() ?> <?= ($this->helper->cleaningHelper->countRememberMe() > 1) ? t('Entries') : t('Entry') ?>
+                    </span>
+                </li>
+            </ul>
+            <ul class="job-result fa-ul">
+                <li class="job-result-title"><i class="fa fa-check fa-li" aria-hidden="true"></i><?= t('Job Result') ?></li>
+                <li class="job-result-text"><?= t('The table will be completely emptied') ?></li>
+            </ul>
+        </div>
+        <div class="form-actions">
+            <a href="<?= $this->url->href('CleaningController', 'confirmRememberMePurge', array('table' => 'remember_me', 'job' => '03', 'plugin' => 'ContentCleaner'), false, '', false) ?>" class="btn job-btn js-modal-confirm"><span class="content-cleaner-icon"></span> <?= t('Purge Login Sessions') ?></a>
+        </div>
+    </fieldset>
+</div>
