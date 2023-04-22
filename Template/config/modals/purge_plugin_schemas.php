50e39fa0bf74771315f5c4fd7b5f6d8888a69d87
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-22 03:10:52 +0100

    Update purge_plugin_schemas.php

--- Template/config/modals/purge_plugin_schemas.php
+++ Template/config/modals/purge_plugin_schemas.php
@@ -1,23 +1,18 @@
-<div class="modal-page-header">
+<div id="PurgePluginSchemasHeader" class="modal-page-header">
     <h2 class="relative">
-        <span class="content-cleaner-icon"></span> <?= t('Automatic Cleaning') ?> <span class="modal-template-id"></span>
+        <span class="content-cleaner-icon"></span> <?= t('Confirm Cleaning Job') ?>
+        <span class="job-number" title="<?= t('Cleaning Job Number') ?>"><?= $job ?></span>
+        <span class="modal-title">
+            <span class="db-settings-icon"></span> <?= t('Plugin Registration Entries') ?> <span class="modal-reset-settings"></span>
         </span>
     </h2>
 </div>
-<div id="DeleteModal" class="modal-contents">
+<div id="PurgePluginSchemasContent" class="modal-contents">
     <div class="confirm">
         <p class="confirm-notice">
-            <?= t('Click the button to purge the database of unused plugin schema data') ?>
-            <div class="template-contents">
-                <div class="template-title"></div>
-            </div>
+            <?= t('Click the button to purge the database of unused plugin registration data') ?>
         </p>
-
-    <?= $this->modal->confirmButtons(
-    'CleaningController',
-    'pluginSchemaPurge',
-    array('plugin' => 'ContentCleaner'),
-    t('Purge')
-) ?>
+        <span id="JobType"><?= t('Automatic Cleaning Jobs') ?></span>
+        <?= $this->modal->confirmButtons('CleaningController', 'pluginSchemaPurge', array('plugin' => 'ContentCleaner'), t('Purge')) ?>
     </div>
 </div>
