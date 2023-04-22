d953b05689142041446d569236e44d3c75f90201
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-22 01:47:13 +0100

    Update reset.php Styling

--- Template/config/modals/reset.php
+++ Template/config/modals/reset.php
@@ -3,21 +3,24 @@
     $outgoingAction = $this->app->getRouterAction();
 ?>
 
-<div class="modal-page-header">
+<div id="ResetModal" class="modal-page-header">
     <h2 class="relative">
-        <span class="content-cleaner-icon"></span> <?= t('Confirm Cleaning Job N° ') . $job ?>
+        <span class="content-cleaner-icon"></span> <?= t('Confirm Cleaning Job') ?>
+        <span class="job-number" title="<?= t('Cleaning Job Number') ?>"><?= $job ?></span>
         <span class="modal-title">
+
             <?php if (($incomingController == 'CleaningController') && ($outgoingAction =='confirmResetCalendar')): ?>
                 <span class="db-settings-icon"></span> <?= t('Reset Settings') ?> <span class="modal-reset-settings"></span>
-
             <?php elseif (($incomingController == 'CleaningController') && ($outgoingAction =='confirmSessionsPurge')): ?>
                 <span class="db-settings-icon"></span> <?= t('Reset Sessions') ?> <span class="modal-reset-settings"></span>
             <?php endif ?>
+
         </span>
     </h2>
 </div>
-<div id="ResetModal" class="modal-contents">
+<div class="modal-contents">
     <div class="confirm">
+
         <?php if (($incomingController == 'CleaningController') && ($outgoingAction =='confirmResetCalendar')): ?>
             <p class="confirm-notice">
                 <?= t('Click the button to restore the calendar settings to the default values') ?>
@@ -30,6 +33,7 @@
             </p>
             <?= $this->modal->confirmButtons('CleaningController', 'resetCalendarSettings', array('plugin' => 'ContentCleaner'), t('Process Job')) ?>
         <?php endif ?>
+
         <?php if (($incomingController == 'CleaningController') && ($outgoingAction =='confirmSessionsPurge')): ?>
             <p class="confirm-notice">
                 <?= t('Click the button to empty the sessions table') ?>
@@ -42,5 +46,6 @@
             </p>
             <?= $this->modal->confirmButtons('CleaningController', 'purgeSessionsData', array('plugin' => 'ContentCleaner'), t('Process Job')) ?>
         <?php endif ?>
+
     </div>
 </div>
