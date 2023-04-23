388b30f0ebe30afd3ec439f5409e5d50fee1aa5b
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-23 11:43:32 +0100

    Move Automatic Cleaning Jobs to automatic-clean.php

---
+++ Template/cleaning-jobs/automatic-clean.php
@@ -0,0 +1,62 @@
+<!-- ContentCleaner: AUTOMATIC CLEANING JOB 01 -->
+<div class="job-section-wrapper">
+    <fieldset class="job-wrapper relative">
+        <legend class="job-title">
+            <span class="content-cleaner-icon"></span> <?= t('Unused Plugin Registration Entries') ?>
+            <!-- ContentCleaner: DO NOT CHANGE JOB NUMBERS -->
+            <span class="job-number-wrapper" title="<?= t('Cleaning Job Number') ?>"><span class="job-number">01</span></span>
+        </legend>
+        <p class="job-desc">
+            <?= t('Plugins which have altered the database also register themselves in the database.') ?>
+        </p>
+        <div class="job-content">
+            <ul class="job-tables fa-ul">
+                <li class="job-tables-title">
+                    <i class="fa fa-table fa-li" aria-hidden="true"></i><?= t('Table Affected') ?>
+                </li>
+                <li class="job-tables-table">plugin_schema_versions</li>
+            </ul>
+            <ul class="job-result fa-ul">
+                <li class="job-result-title"><i class="fa fa-check fa-li" aria-hidden="true"></i><?= t('Job Result') ?></li>
+                <li class="job-result-text"><?= t('The table is checked for all unknown entries compared to your installed plugins. Use this job if you are having issues reinstalling a plugin.') ?></li>
+            </ul>
+        </div>
+        <div class="form-actions">
+            <a href="<?= $this->url->href('CleaningController', 'confirmPluginSchemaPurge', array('job' => '01', 'plugin' => 'ContentCleaner'), false, '', false) ?>" class="btn job-btn js-modal-confirm">
+                <span class="db-delete-icon"></span> <?= t('Purge Unused Plugin Entries') ?>
+            </a>
+        </div>
+    </fieldset>
+</div>
+
+<!-- ContentCleaner: AUTOMATIC CLEANING JOB 02 -->
+<div class="job-section-wrapper">
+    <fieldset class="job-wrapper relative">
+        <legend class="job-title">
+            <span class="content-cleaner-icon"></span> <?= t('Clean All Unknown Tables and Columns') ?>
+            <!-- ContentCleaner: DO NOT CHANGE JOB NUMBERS -->
+            <span class="job-number-wrapper" title="<?= t('Cleaning Job Number') ?>"><span class="job-number">02</span></span>
+        </legend>
+        <p class="job-desc">
+            <?= t('This job is an all-in-one process to completely deep clean your database.') ?>
+        </p>
+        <div class="job-content">
+            <ul class="job-tables fa-ul">
+                <li class="job-tables-title">
+                    <i class="fa fa-table fa-li" aria-hidden="true"></i><?= t('Table Affected') ?>
+                </li>
+                <li class="job-tables-table">plugin_schema_versions</li>
+                <li class="more"><?= t('+ more') ?></li>
+            </ul>
+            <ul class="job-result fa-ul">
+                <li class="job-result-title"><i class="fa fa-check fa-li" aria-hidden="true"></i><?= t('Job Result') ?></li>
+                <li class="job-result-text"><?= t('First all unknown tables are identified and deleted followed by all unknown columns in each table. Finally the plugin registration entries are purged.') ?></li>
+            </ul>
+        </div>
+        <div class="form-actions">
+            <a href="<?= $this->url->href('CleaningController', 'confirmAutoPurgeAndClean', array('job' => '02', 'plugin' => 'ContentCleaner'), false, '', false) ?>" class="btn job-btn js-modal-confirm">
+                <span class="db-delete-icon"></span> <?= t('Deep Clean Database') ?>
+            </a>
+        </div>
+    </fieldset>
+</div>
