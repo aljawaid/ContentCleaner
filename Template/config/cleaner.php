58626001c53cca1ed34d6517f0ccc88da9d1006d
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-27 08:05:01 +0100

    Better wording for accordion sections

--- Template/config/cleaner.php
+++ Template/config/cleaner.php
@@ -108,7 +108,7 @@
             <span class="plugin-icon"></span> <?= t('3 - Plugin Cleaning Jobs') ?>
         </summary>
         <div class="accordion-content">
-            <p class="section-intro"><?= t('Jobs in this section affect leftover tables which were created by plugins. Once uninstalled, all tables will be marked as unknown. Process each job carefully only if you have installed the relevant plugin previously. Deleting plugin related database tables before uninstalling the plugin will prompt the plugin to recreate the tables, not uninstall the plugin.') ?></p>
+            <p class="section-intro"><?= t('Jobs in this section affect leftover tables which were created by plugins. Once a plugin is uninstalled, all leftover tables for the uninstalled plugin will be marked as unknown. Process each job carefully only if you have installed the relevant plugin previously. Deleting plugin related database tables before uninstalling the plugin will prompt the plugin to recreate the tables, not uninstall the plugin.') ?></p>
             <?= $this->render('ContentCleaner:cleaning-jobs/plugin-clean') ?>
         </div>
     </details>
