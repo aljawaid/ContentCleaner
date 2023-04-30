2c0f8ee50871308ee85e031e2d1e17f9e74b4bcb
IJ <jawaid_imran@hotmail.com>
2023-04-30 15:20:09 +0100

    Add PluginCleaningModel.php
    #11

---
+++ Model/PluginCleaningModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Kanboard\Plugin\ContentCleaner\Model;
+
+use Kanboard\Core\Base;
+
+/**
+ * Class Kanboard\Plugin\ContentCleaner\Model;
+ *
+ * @author aljawaid
+ */
+
+class PluginCleaningModel extends Base
+{
+
+}
