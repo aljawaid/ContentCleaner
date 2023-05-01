ac273a58ed268a2dbfdffae280b248f8b6796872
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-05-01 21:19:14 +0100

    Move `getDeletablePlugins` to ContentCleanerController.php
    - #11
    - thanks @creecros

--- Controller/ContentCleanerController.php
+++ Controller/ContentCleanerController.php
@@ -31,4 +31,14 @@ class ContentCleanerController extends \Kanboard\Controller\PluginController
             'db_version' => $this->db->getDriver()->getDatabaseVersion(),
         )));
     }
+
+    public function getDeletablePlugins()
+    {
+        // GET THE JSON OF PLUGINS WHICH CAN BE DELETED
+        // 'template/cleaning-jobs/plugin-cleaning.json'
+
+        $file = PLUGINS_DIR . '/ContentCleaner/Template/cleaning-jobs/plugin-cleaning.json';
+        $plugins = json_decode(file_get_contents($file), true);
+        return $plugins;
+    }
 }
--- Controller/PluginCleaningController.php
+++ Controller/PluginCleaningController.php
@@ -14,13 +14,5 @@ use Kanboard\Core\Controller\PageNotFoundException;
 
 class PluginCleaningController extends BaseController
 {
-    public function getDeletablePlugins()
-    {
-        // GET THE JSON OF PLUGINS WHICH CAN BE DELETED
-        // 'template/cleaning-jobs/plugin-cleaning.json'
 
-        $file = PLUGINS_DIR . '/ContentCleaner/Template/cleaning-jobs/plugin-cleaning.json';
-        $plugins = json_decode(file_get_contents($file), true);
-        return $plugins;
-    }
 }
