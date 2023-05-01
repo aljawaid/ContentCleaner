a32b3291f2e8aee21181f0357d198e7eca0bf7e9
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-05-01 08:24:34 +0100

    Create PluginCleaningController.php
    #11

---
+++ Controller/PluginCleaningController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kanboard\Plugin\ContentCleaner\Controller;
+
+use Kanboard\Controller\BaseController;
+use Kanboard\Core\Plugin\Directory;
+use Kanboard\Core\Controller\PageNotFoundException;
+use Kanboard\Core\Http\Client;
+
+/**
+ * Plugin ContentCleaner
+ * Class PluginCleaningController
+ * @author aljawaid
+ */
+
+class PluginCleaningController extends BaseController
+{
+}
