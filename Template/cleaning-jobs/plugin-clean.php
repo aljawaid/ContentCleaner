3a15fc4a3c9d07d812723a46c7b52ff67cf6d362
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-05-06 11:26:46 +0100

    Rename variable for plugin name

--- Template/cleaning-jobs/plugin-clean.php
+++ Template/cleaning-jobs/plugin-clean.php
@@ -1,15 +1,15 @@
 <?php foreach ($deletable_plugins as $plugin): ?>
     <!-- ContentCleaner: PLUGIN CLEANING JOB 01 -->
-    <?php $plugin_job_1 = 'Plugin'; ?>
+    <?php $plugin_job_name = $plugin['plugin_title']; ?>
     <div class="job-section-wrapper">
         <fieldset class="job-wrapper plugin-job-wrapper">
             <legend class="job-title">
-                <span class="content-cleaner-icon"></span> <?= t('%s Plugin', $plugin_job_1) ?>
+                <span class="content-cleaner-icon"></span> <?= t('%s Plugin', $plugin_job_name) ?>
                 <!-- ContentCleaner: DO NOT CHANGE JOB NUMBERS -->
                 <span class="job-number-wrapper" title="<?= t('Cleaning Job Number') ?>"><span class="job-number">01</span></span>
             </legend>
             <p class="job-desc">
-                <?= t('Remove all traces, content and plugin registration data for %s.', $plugin_job_1) ?>
+                <?= t('Remove all traces, content and plugin registration data for %s.', $plugin_job_name) ?>
                 <span class="tooltip content-cleaner-tooltip">
                     <i class="fa fa-info-circle" aria-hidden="true"></i>
                     <script type="text/template">
@@ -72,11 +72,11 @@
                 </ul>
                 <ul class="job-result fa-ul">
                     <li class="job-result-title"><i class="fa fa-check fa-li" aria-hidden="true"></i><?= t('Job Result') ?></li>
-                    <li class="job-result-text"><?= t('%s will not exist on your system.', $plugin_job_1) ?></li>
+                    <li class="job-result-text"><?= t('%s will not exist on your system in any form.', $plugin_job_name) ?></li>
                 </ul>
             </div>
             <div class="form-actions">
-                <a href="<?= $this->url->href('PluginCleaningController', 'confirmRemoval', array('plugin_job_name' => $plugin_job_1, 'job' => '01', 'plugin' => 'ContentCleaner'), false, '', false) ?>" class="btn job-btn js-modal-confirm"><span class="content-cleaner-icon"></span> <?= t('Deep Clean Plugin') ?></a>
+                <a href="<?= $this->url->href('PluginCleaningController', 'confirmRemoval', array('plugin_job_name' => $plugin_job_name, 'job' => '01', 'plugin' => 'ContentCleaner', 'plugin' => $plugin), false, '', false) ?>" class="btn job-btn js-modal-confirm"><span class="content-cleaner-icon"></span> <?= t('Deep Clean Plugin') ?></a>
             </div>
         </fieldset>
     </div>
