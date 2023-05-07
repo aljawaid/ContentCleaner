5452074b765ae47780818d4b955d2af60410744b
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-05-07 18:03:41 +0100

    Fix Variables in Template
    024a4cfa5af55202ad62ed24aaab75e2d07f0fcb

--- Template/config/modals/plugin_remove.php
+++ Template/config/modals/plugin_remove.php
@@ -1,6 +1,7 @@
 <div id="RemovePluginHeader" class="modal-page-header">
     <h2 class="relative">
-        <span class="content-cleaner-icon"></span> <?= t('Content Cleaner') ?>
+        <span class="content-cleaner-icon"></span> <?= t('Confirm Cleaning Job') ?>
+        <span class="job-number" title="<?= t('Cleaning Job Number') ?>"><?= $job_number ?></span>
         <span class="modal-title">
             <span class="plugin-icon"></span> <?= t('Deep Clean Plugin') ?> <span class="modal-reset-settings"></span>
         </span>
@@ -9,9 +10,9 @@
 <div id="RemovePluginContent" class="modal-contents">
     <div class="confirm">
         <p class="confirm-notice">
-            <?= t('Click the button to completely remove this plugin from the database') ?>
+            <?= t('Click the button to completely remove %s from the database', $plugin_job_name) ?>
         </p>
-        <span id="JobType"><?= t('Deep Cleaning') ?></span>
-        <?= $this->modal->confirmButtons('PluginCleaningController', 'removePlugin', array('table' => $table, 'plugin' => 'ContentCleaner'), t('Delete Table')) ?>
+        <span id="JobType"><?= t('Plugin Cleaning') ?></span>
+        <?= $this->modal->confirmButtons('PluginCleaningController', 'removePlugin', array('job_number' => $job_number, 'plugin_job_name' => $plugin_job_name, 'plugin' => 'ContentCleaner'), t('Delete Table')) ?>
     </div>
 </div>
