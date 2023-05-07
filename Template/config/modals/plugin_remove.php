389e96d1e9d67692e0fa4a067141522f9a9dc313
IJ <jawaid_imran@hotmail.com>
2023-05-07 15:31:04 +0100

    Update plugin_remove.php

--- Template/config/modals/plugin_remove.php
+++ Template/config/modals/plugin_remove.php
@@ -1,3 +1,17 @@
-<div class="">
-    <?= t('Remove Plugin') ?>
+<div id="RemovePluginHeader" class="modal-page-header">
+    <h2 class="relative">
+        <span class="content-cleaner-icon"></span> <?= t('Content Cleaner') ?>
+        <span class="modal-title">
+            <span class="plugin-icon"></span> <?= t('Deep Clean Plugin') ?> <span class="modal-reset-settings"></span>
+        </span>
+    </h2>
+</div>
+<div id="RemovePluginContent" class="modal-contents">
+    <div class="confirm">
+        <p class="confirm-notice">
+            <?= t('Click the button to completely remove this plugin from the database') ?>
+        </p>
+        <span id="JobType"><?= t('Deep Cleaning') ?></span>
+        <?= $this->modal->confirmButtons('PluginCleaningController', 'removePlugin', array('table' => $table, 'plugin' => 'ContentCleaner'), t('Delete Table')) ?>
+    </div>
 </div>
