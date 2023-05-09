8e3913bd07a42f07cfc1f20dca2f143ce1ea7df0
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-05-09 21:59:55 +0100

    Add Deep Clean Plugin Tables to plugin_deep_clean.php
    - #12

--- Template/config/modals/plugin_deep_clean.php
+++ Template/config/modals/plugin_deep_clean.php
@@ -21,11 +21,24 @@
             <?php $deletable_plugins = $this->helper->pluginCleaningHelper->getDeletablePlugins(); ?>
             <?php foreach ($deletable_plugins as $plugin): ?>
                 <?php if ($plugin['plugin_title'] == $plugin_job_name): ?>
+                    <!-- ContentCleaner: PLUGIN DEEP CLEAN PLUGIN TABLES -->
+                    <?php if (($incomingController == 'PluginCleaningController') && ($outgoingAction == 'confirmDeletePluginTables')): ?>
+                        <?php if (isset($plugin['plugin_tables'])): ?>
+                            <li class="plugin-job-delete-item">
+                                <i class="fa fa-check fa-li pp-green" aria-hidden="true"></i>
+                                <i class="fa fa-table" aria-hidden="true"></i> <?= t('Plugin Tables') ?>
+                            </li>
+                        <?php endif ?>
+                    <?php endif ?>
                 <?php endif ?>
             <?php endforeach ?>
         </ul>
         <span id="JobType">
             <span class="plugin-icon"></span> <?= t('Plugin Deep Cleaning') ?>
         </span>
+        <!-- ContentCleaner: PLUGIN DEEP CLEAN PLUGIN TABLES -->
+        <?php if (($incomingController == 'PluginCleaningController') && ($outgoingAction == 'confirmDeletePluginTables')): ?>
+            <?= $this->modal->confirmButtons('PluginCleaningController', 'deletePluginTables', array('plugin_name' => $plugin_name, 'plugin' => 'ContentCleaner'), t('Deep Clean Plugin')) ?>
+        <?php endif ?>
     </div>
 </div>
