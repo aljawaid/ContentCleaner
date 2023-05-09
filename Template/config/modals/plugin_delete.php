44a87b98d4150ef04e22870616ded665ec9ebe8a
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-05-09 12:30:50 +0100

    Add Job Actions to plugin_delete.php
    - #11

--- Template/config/modals/plugin_delete.php
+++ Template/config/modals/plugin_delete.php
@@ -12,6 +12,36 @@
         <p class="confirm-notice">
             <?= t('Process this job to completely remove %s from the database.', $plugin_job_name) ?>
         </p>
+        <ul class="plugin-job-delete fa-ul">
+            <div class="plugin-job-delete-title"><?= t('Job Actions') ?></div>
+            <?php $deletable_plugins = $this->helper->pluginCleaningHelper->getDeletablePlugins(); ?>
+            <?php foreach ($deletable_plugins as $plugin): ?>
+                <?php if ($plugin['plugin_title'] == $plugin_job_name): ?>
+                    <?php if (isset($plugin['plugin_tables'])): ?>
+                        <li class="plugin-job-delete-item">
+                            <i class="fa fa-check fa-li pp-green" aria-hidden="true"></i>
+                            <i class="fa fa-table" aria-hidden="true"></i> <?= t('Plugin Tables') ?>
+                        </li>
+                    <?php endif ?>
+                    <?php if (isset($plugin['core_table_columns'])): ?>
+                        <li class="plugin-job-delete-item">
+                            <i class="fa fa-check fa-li pp-green" aria-hidden="true"></i>
+                            <i class="fa fa-columns" aria-hidden="true"></i> <?= t('Core Table Columns') ?>
+                        </li>
+                    <?php endif ?>
+                    <?php if (isset($plugin['core_table_entries'])): ?>
+                        <li class="plugin-job-delete-item">
+                            <i class="fa fa-check fa-li pp-green" aria-hidden="true"></i>
+                            <i class="fa fa-tasks" aria-hidden="true"></i> <?= t('Core Table Entries') ?>
+                        </li>
+                    <?php endif ?>
+                <?php endif ?>
+            <?php endforeach ?>
+            <li class="plugin-job-delete-item">
+                <i class="fa fa-check fa-li pp-green" aria-hidden="true"></i>
+                <span class="db-database-icon"></span> <?= t('Plugin Schema Registration Entry') ?>
+            </li>
+        </ul>
         <span id="JobType">
             <span class="plugin-icon"></span> <?= t('Plugin Cleaning') ?>
         </span>
