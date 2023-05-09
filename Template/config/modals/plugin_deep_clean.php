d5c16dade4c0dbb7b5609bfd0a7d7c6db94d6827
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-05-09 21:57:52 +0100

    Add Modal Template to plugin_deep_clean.php
    - #12

---
+++ Template/config/modals/plugin_deep_clean.php
@@ -0,0 +1,31 @@
+<?php
+    $incomingController = $this->app->getRouterController();
+    $outgoingAction = $this->app->getRouterAction();
+?>
+
+<div id="RemovePluginHeader" class="modal-page-header">
+    <h2 class="relative">
+        <span class="content-cleaner-icon"></span> <?= t('Confirm Deep Clean') ?>
+        <span class="modal-title">
+            <span class="plugin-icon"></span> <?= t('Deep Clean Plugin') ?> <span class="modal-reset-settings"></span>
+        </span>
+    </h2>
+</div>
+<div id="RemovePluginContent" class="modal-contents">
+    <div class="confirm">
+        <p class="confirm-notice">
+            <?= t('Process this job to complete the actions listed below for %s.', $plugin_job_name) ?>
+        </p>
+        <ul class="plugin-job-delete fa-ul">
+            <div class="plugin-job-delete-title"><?= t('Job Actions') ?></div>
+            <?php $deletable_plugins = $this->helper->pluginCleaningHelper->getDeletablePlugins(); ?>
+            <?php foreach ($deletable_plugins as $plugin): ?>
+                <?php if ($plugin['plugin_title'] == $plugin_job_name): ?>
+                <?php endif ?>
+            <?php endforeach ?>
+        </ul>
+        <span id="JobType">
+            <span class="plugin-icon"></span> <?= t('Plugin Deep Cleaning') ?>
+        </span>
+    </div>
+</div>
