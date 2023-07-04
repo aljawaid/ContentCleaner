6d0c51a0981d4ba85498c4c049f6f42db5da7dbb
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-07-04 20:29:10 +0100

    Add `getFolderName` to Helper

--- Helper/PluginCleaningHelper.php
+++ Helper/PluginCleaningHelper.php
@@ -15,4 +15,28 @@ class PluginCleaningHelper extends Base
         $plugins = json_decode(file_get_contents($file), true);
         return $plugins;
     }
+
+    /**
+     * Get the folder name of the Plugin
+     *
+     * @param   $plugin_name        string
+     * @return  $folder_name        array
+     * @author  aljawaid
+     */
+    public function getFolderName()
+    {
+        // Pull variable from modal
+        $plugin_name = $this->request->getStringParam('plugin_name');
+        $folder_name = [];
+
+        // Match variable to json content if the plugin name matches
+        foreach ($this->helper->pluginCleaningHelper->getDeletablePlugins() as $plugin) {
+            if ($plugin['plugin_name'] === $plugin_name && isset($plugin['plugin_folder_name'])) {
+                $folder_name = $plugin['plugin_folder_name'];
+                break;
+            }
+        }
+
+        return $folder_name;
+    }
 }
