a5db9fc27ae5a605c5e1a58feea9a352035b1ad5
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-07-05 13:18:44 +0100

    Restructure Remove Schema Modal

--- Template/config/modals/remove_plugin_schema.php
+++ Template/config/modals/remove_plugin_schema.php
@@ -2,6 +2,7 @@
     /* MODAL SIZE */
     #modal-box {
         width: auto !important;
+        min-width: 500px;
         overflow: hidden;
     }
 
@@ -33,28 +34,37 @@
     }
 </style>
 
-<div class="delete-schema-modal">
-    <div class="cc-page-header">
-        <h2 class=""><?= $title ?></h2>
+<div id="RemovePluginSchemaModal" class="modal">
+    <div class="modal-page-header">
+        <h2 class="relative">
+            <span class="modal-title">
+                <span class="content-cleaner-icon"></span> <?= $title ?>
+            </span>
+        </h2>
     </div>
-    <div class="confirm">
-        <p class="confirm-delete">
-            <?= e('Do you really want to remove the entry for %s?', '<strong>' . $plugin_job_name . '</strong>') ?>
-        </p>
-        <?php $folder_name = $this->helper->pluginCleaningHelper->getFolderName() ?>
-        <?php if (file_exists('plugins/' . $folder_name . '')): ?>
-            <section class="message error cleaner-warning">
-                <header></header>
-                <i class=""></i>
-                <h3 class="">
-                    <span class="message-title"><?= t('Warning') ?></span>
-                    <span class="message-text"><?= t('%s is detected as installed. You should uninstall %s before completing this action to avoid the automatic creation of the database entry.', $plugin_job_name, $plugin_job_name) ?></span>
-                </h3>
-            </section>
-        <?php endif ?>
-        <div class="form-actions">
-            <?= $this->url->link(t('Deep Clean'), 'PluginCleaningController', 'deletePluginSchemaEntry', array('plugin_name' => $plugin_name, 'plugin' => 'ContentCleaner'), true, 'btn btn-ab-delete') ?>
-            <button class="btn cancel-btn js-modal-close" href="#"><?= t('Cancel') ?></button>
+    <div id="RemovePluginSchema" class="modal-contents">
+        <div class="confirm warning-confirm">
+            <?php $folder_name = $this->helper->pluginCleaningHelper->getFolderName() ?>
+            <?php if (file_exists('plugins/' . $folder_name . '')): ?>
+                <section class="message error cleaner-warning">
+                    <header></header>
+                    <i class=""></i>
+                    <h3 class="">
+                        <span class="message-title"><?= t('Warning') ?></span>
+                        <span class="message-text"><?= t('%s is detected as installed. You should uninstall %s before completing this action to avoid the automatic creation of the database entry.', $plugin_job_name, $plugin_job_name) ?></span>
+                    </h3>
+                </section>
+            <?php endif ?>
+            <p class="confirm-notice">
+                <?= e('Do you really want to remove the entry for %s?', '<strong>' . $plugin_job_name . '</strong>') ?>
+            </p>
+            <span id="JobType">
+                <span class="plugin-icon"></span> <?= t('Plugin Deep Cleaning') ?>
+            </span>
         </div>
+    </div>
+    <div class="modal-actions">
+        <?= $this->url->link(t('Deep Clean'), 'PluginCleaningController', 'deletePluginSchemaEntry', array('plugin_name' => $plugin_name, 'plugin' => 'ContentCleaner'), true, 'btn btn-cc-delete') ?>
+        <button class="btn cancel-btn js-modal-close" href="#"><?= t('Cancel') ?></button>
     </div>
 </div>
