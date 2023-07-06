aeffaaee8e22669365a891a478509eb07b8c5219
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-07-06 06:47:02 +0100

    Fix Controller File Comments

--- Controller/ContentCleanerController.php
+++ Controller/ContentCleanerController.php
@@ -8,21 +8,21 @@ use Kanboard\Core\Plugin\Directory;
 /**
  * Plugin ContentCleaner
  * Class ContentCleanerController
- * @author aljawaid
+ *
+ * @package  ContentCleanerController
+ * @author   aljawaid
  */
 
 class ContentCleanerController extends \Kanboard\Controller\PluginController
 {
     /**
      * Display the Settings Page
-     * Use this function to create a page showing your template content.
-     * This function must be checked with 'Views - Add Menu Item - Template Hook' in Plugin.php
-     * This function must be checked with 'Extra Page - Routes' in Plugin.php
+     *
+     *
      * Use: $this->helper->layout->config for config settings menu sidebar
      * Use: $this->helper->layout->plugin for plugin menu sidebar
      * @access public
      */
-
     public function show()
     {
         $this->response->html($this->helper->layout->config('contentCleaner:config/cleaner', array(
